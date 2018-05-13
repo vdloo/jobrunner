9ac48fb792d329a26828ced40667f67de0f9f8a6
Rick van de Loo <rickvandeloo@gmail.com>
2018-05-13 11:45:51 +0200

    update default backend connection details

--- jobrunner/settings.py
+++ jobrunner/settings.py
@@ -4,19 +4,24 @@ from socket import gethostname
 
 from os.path import realpath
 
+from subprocess import check_output
+
 top_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                        os.pardir))
 PROJECT_DIR = join(dirname(dirname(realpath(__file__))))
-# todo: get the IP from the ENV or a flag
-REDIS_HOST = '172.17.0.1'  # Docker gateway
-MYSQL_HOST = '172.17.0.1'  # Docker gateway
+BACKEND_HOST = check_output(
+    "consul members | grep retropie | awk '{print$2}' | awk -F'[][]' '{print $2}'",
+    shell=True
+).decode('utf-8').strip()
+REDIS_HOST = BACKEND_HOST
+MYSQL_HOST = BACKEND_HOST
 JOBBOARD_CONF = {
     'board': 'redis',
     'host': REDIS_HOST
 }
 PERSISTENCE_CONF = {
-    "connection": "mysql://root:taskflow@[{}]"
-                  "/taskflow".format(MYSQL_HOST),
+    "connection": "mysql://simulacra@[{}]"
+                  "/simulacra".format(MYSQL_HOST),
 }
 LOGBOOK_NAME = 'jobrunner'
 SHOW_POLLERS = 20
